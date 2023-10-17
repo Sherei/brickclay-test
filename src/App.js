f52b08f983be0748cf93fc6eef7f677d6b62303f
seoalirajpoot <seo.alirajpoot@gmail.com>
2023-10-17 21:38:15 +0500

    lottie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import { FaArrowUp } from "react-icons/fa";
-
 import Hero from "./components/hero/Hero";
 import Experience from "./components/experience/Experience";
 import About from "./components/about/About";
@@ -11,11 +9,34 @@ import Footer from "./components/footer/Footer";
 import Testimonial from "./components/testimonial/Testimonial";
 import Service from "./components/service/Service";
 import { ToastContainer } from 'react-toastify';
-
 import 'react-toastify/dist/ReactToastify.css';
+import Lottie from 'lottie-react';
+import Nointernet from "./components/Animations/Nointernet.json"
+import "./App.css";
 
 function App() {
+
   const [showTopButton, setShowTopButton] = useState(false);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    function onlineHandler() {
+      setIsOnline(true);
+    }
+
+    function offlineHandler() {
+      setIsOnline(false);
+    }
+
+    window.addEventListener('online', onlineHandler);
+    window.addEventListener('offline', offlineHandler);
+
+    return () => {
+      window.removeEventListener('online', onlineHandler);
+      window.removeEventListener('offline', offlineHandler);
+    };
+  }, []);
+
 
   const handleScroll = () => {
     if (window.scrollY > 100) {
@@ -28,33 +49,43 @@ function App() {
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth", // Optional: Scroll behavior animation
+      behavior: "smooth", 
     });
   };
 
-  // Add a scroll event listener to check when to show the button
   window.addEventListener("scroll", handleScroll);
+  if (isOnline) {
+    return (
+      <>
+        <div style={{ overflow: "hidden" }}>
+          {showTopButton && (
+            <button className="btn top_btn" onClick={scrollToTop}>
+              <FaArrowUp />
+            </button>
+          )}
+          <Hero />
+          <Experience />
+          <About />
+          <Faq />
+          <Testimonial />
+          <Stats />
+          <Service />
+          <Footer />
+          <ToastContainer />
+        </div>
 
+      </>
+    );
+  }
   return (
-    <>
-      <div style={{ overflow: "hidden" }}>
-        {showTopButton && (
-          <button className="btn top_btn" onClick={scrollToTop}>
-            <FaArrowUp />
-          </button>
-        )}
-        <Hero />
-        <Experience />
-        <About />
-        <Faq />
-        <Testimonial />
-        <Stats />
-        <Service />
-        <Footer />
-        <ToastContainer />
+    <div className='d-flex flex-column justify-content-center align-items-center' style={{ minHeight: '50vh' }}>
+      <div>
+        <Lottie animationData={Nointernet} loop={true} />;
       </div>
-
-    </>
+      <button className='btn btn-primary' onClick={() => window.location.reload()}>
+        Reload
+      </button>
+    </div>
   );
 }
 
